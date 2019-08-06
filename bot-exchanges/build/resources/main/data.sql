1d9064dea90afe9e59246e2ba7aee9f0dd9dce24
Thiago Cavalcanti Reis <thiagocavalcantireis@gmail.com>
2019-08-05 23:39:29 -0300

    Removed build path

--- bot-exchanges/build/resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `Exchange` VALUES (1,1,'Bittrex');
-
-INSERT INTO `Authentication` VALUES ('1',1,'API_KEY','API_SECRET','USER_ID',1);
--- bot-gateway/build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `AuthenticationRule` VALUES (1,'ROLE_ADMIN');
-
-INSERT INTO `User` VALUES (1,1,'thiagocavalcantireis@gmail.com','Thiago','Reis','$2a$12$hYbbylCEZv4QKB4L0Q4X6O.RBhFO.X2gioEdeIe0VB.TJiMdBCzfu','treis');
-
-INSERT INTO `User_AuthenticationRule` VALUES (1,1,1);
