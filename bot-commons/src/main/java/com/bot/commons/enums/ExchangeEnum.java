82e02188d1d20f9d53af9fa487e5f535a3c1729a
Thiago Cavalcanti Reis <thiagocavalcantireis@gmail.com>
2019-09-02 00:59:20 -0300

    Refactoring response objects from the exchange clients

--- bot-commons/src/main/java/com/bot/commons/enums/ExchangeEnum.java
+++ bot-commons/src/main/java/com/bot/commons/enums/ExchangeEnum.java
@@ -1,29 +1,20 @@
 package com.bot.commons.enums;
 
-import static com.bot.commons.utils.DateUtils.PARSE_BINANCE_DATE;
-import static com.bot.commons.utils.DateUtils.PARSE_BITTREX_DATE;
-
 import lombok.Getter;
-
-import java.time.ZonedDateTime;
-import java.time.temporal.TemporalAmount;
-import java.util.function.BiFunction;
 
 @Getter
 public enum ExchangeEnum {
 
-    POLONIEX((long) 1, "Poloniex", null),
-    BITTREX((long) 2, "Bittrex", PARSE_BITTREX_DATE),
-    BLEUTRADE((long) 3, "Bleutrade", null),
-    BINANCE((long) 4, "Binance", PARSE_BINANCE_DATE);
+    POLONIEX((long) 1, "Poloniex"),
+    BITTREX((long) 2, "Bittrex"),
+    BLEUTRADE((long) 3, "Bleutrade"),
+    BINANCE((long) 4, "Binance");
 
     private Long id;
     private String cryptoCompareName;
-    private BiFunction<String, TemporalAmount, ZonedDateTime> parseDateFunction;
 
-    ExchangeEnum(Long id, String cryptoCompareName, BiFunction<String, TemporalAmount, ZonedDateTime> parseDateFunction) {
+    ExchangeEnum(Long id, String cryptoCompareName) {
         this.id = id;
         this.cryptoCompareName = cryptoCompareName;
-        this.parseDateFunction = parseDateFunction;
     }
 }
--- bot-commons/src/main/java/com/bot/commons/utils/DateUtils.java
+++ bot-commons/src/main/java/com/bot/commons/utils/DateUtils.java
@@ -7,7 +7,6 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAmount;
-import java.util.function.BiFunction;
 
 public class DateUtils {
 
@@ -15,13 +14,6 @@ public class DateUtils {
 
     public static final DateTimeFormatter DATETIME_WITH_T_FORMAT_DESERIALIZE = DateTimeFormatter
             .ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-
-    public static final BiFunction<String, TemporalAmount, ZonedDateTime> PARSE_BINANCE_DATE = (time, tickDuration) -> {
-        ZonedDateTime zonedDateTime = convertEpochMilliToZonedDateTime(time).plus(tickDuration);
-        return zonedDateTime.minusMinutes(zonedDateTime.getMinute() % (tickDuration.get(ChronoUnit.SECONDS) / 60))
-                .withSecond(0)
-                .withNano(0);
-    };
 
     public static ZonedDateTime convertEpochMilliToZonedDateTime(String time) {
         Instant instant = Instant.ofEpochMilli(Long.parseLong(time));
@@ -33,28 +25,15 @@ public class DateUtils {
         return ZonedDateTime.ofInstant(instant, DEFAULT_ZONE_OFFSET);
     }
 
+    public static ZonedDateTime convertDateToZonedDateTime(String time, DateTimeFormatter dateTimeFormatter) {
+        time = time.split("\\.")[0].replaceAll("\"", "");
+        LocalDateTime datetime = LocalDateTime.parse(time, dateTimeFormatter);
+        return datetime.atZone(DEFAULT_ZONE_OFFSET);
+    }
+
     public static ZonedDateTime roundZonedDateTime(ZonedDateTime zonedDateTime, TemporalAmount tickDuration) {
         return zonedDateTime.minusMinutes(zonedDateTime.getMinute() % (tickDuration.get(ChronoUnit.SECONDS) / 60))
                 .withSecond(0)
                 .withNano(0);
-    }
-
-    public static final BiFunction<String, TemporalAmount, ZonedDateTime> PARSE_BITTREX_DATE = (time, tickDuration) -> {
-        return convertDateToZonedDateTime(time, DATETIME_WITH_T_FORMAT_DESERIALIZE).plus(tickDuration);
-    };
-
-    public static ZonedDateTime convertDateToZonedDateTime(String time, DateTimeFormatter dateTimeFormatter) {
-        time = time.split("\\.")[0].replaceAll("\"", "");
-        LocalDateTime datetime = LocalDateTime.parse(time, DATETIME_WITH_T_FORMAT_DESERIALIZE);
-        return datetime.atZone(DEFAULT_ZONE_OFFSET);
-    }
-
-    public static String convertCryptoCompareDateToBittrexDate(String time, TemporalAmount tickDuration) {
-        Instant instant = Instant.ofEpochSecond(Long.parseLong(time));
-        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, DEFAULT_ZONE_OFFSET).plus(tickDuration);
-        zonedDateTime  = zonedDateTime.minusMinutes(zonedDateTime.getMinute() % (tickDuration.get(ChronoUnit.SECONDS) / 60))
-                .withSecond(0)
-                .withNano(0);
-        return zonedDateTime.format(DATETIME_WITH_T_FORMAT_DESERIALIZE);
     }
 }
